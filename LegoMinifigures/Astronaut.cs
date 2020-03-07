2e0f4b63ce01d5aeffc360b0346e468e437bc201
Monica Djunaidi <modjun12@gmail.com>
2020-03-07 13:15:42 -0600

    refactored code

---
+++ LegoMinifigures/Astronaut.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegoMinifigures
+{
+    class Astronaut
+    {
+        // public property. Below is using enum type within property
+        public Location Location { get; set; }
+
+        // expression bodied property. Below means ALWAYS suited up. No setter. Saying if Name == "Space Person", it is always suited up
+        public bool SuitedUp => Name == "Space Person";
+
+        // read only property and can only assign value once class is instantiated
+        public string Name { get; }
+
+        // Public property with private setter. can have set as private and be updated in a method
+        public string Job { get; private set; }
+        public string Age { get; set; }
+        public int OxygenLevel { get; set; }
+        
+        // when class gets called, need to specify name and job to use instantiated class
+        public Astronaut(string name, string job)
+        {
+            Name = name;
+            Job = job;
+        }
+        public void DoYourJob()
+        {
+            Console.WriteLine($"{Name} is doing all their {Job} duties");
+        }
+        public void Promote()
+        {
+            Job = "Commander of Janitors";
+        }
+    }
+}
---
+++ LegoMinifigures/Location.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegoMinifigures
+{
+    // enum have name and values that are options. Enum are actually integers under the covers. Enum is numbered like an array, but not an array. It's a collection of options. Can also set position value of location, i.e. Earth = 1. Can set it as 1 first so there is no default value when nothing has been set yet. Implicitly internal
+    enum Location
+    {
+        Earth,
+        Space,
+        SpaceStation
+    }
+}
