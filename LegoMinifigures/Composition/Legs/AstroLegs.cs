481ba9111d17b5c5b7d71bd9a49959f8dda6b318
Monica Djunaidi <modjun12@gmail.com>
2020-03-09 21:27:58 -0500

    added composition

---
+++ LegoMinifigures/Composition/Legs/AstroLegs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegoMinifigures.Composition.Legs
+{
+    class AstroLegs
+    {
+        public bool HasPants { get; set; }
+        public ShoeType Shoes { get; set; }
+
+        // creating random generator field
+        Random _rng = new Random(4);
+        public void Walk(int steps)
+        {
+            Console.WriteLine($"The astronaut legs moonwalked {steps} steps.");
+        }
+
+        // below method is returning something
+        public int Jump()
+        {
+            var heightOfJump = _rng.Next();
+            return heightOfJump;
+        }
+    }
+
+    enum ShoeType
+    {
+        None,
+        Boots,
+        FlipFlops
+    }
+}
---
+++ LegoMinifigures/Composition/Torsos/AstroTorso.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegoMinifigures.Composition.Torsos
+{
+    class AstroTorso
+    {
+        public int NumberOfArms { get; set; }
+        public bool ChiseledAbs { get; set; }
+        public bool Shirted { get; set; }
+        public HandType HandType { get; set; }
+        
+        public void Flex()
+        {
+            if (ChiseledAbs && !Shirted)
+            {
+                Console.WriteLine("Check out my sick abs!");
+            }
+            else if (HandType == HandType.XBuster)
+            {
+                Console.WriteLine("Welcome to the gun show!");
+            }
+            else
+            {
+                Console.WriteLine("Weird flex, but ok");
+            }
+        }
+    }
+
+    enum HandType
+    {
+        Human,
+        Robot,
+        Baby,
+        Tentacles,
+        CanadaArm,
+        XBuster
+    }
+}
